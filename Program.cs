aa3ae13fc74c9a6644393999cbc520ed7e1e2548
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-05 23:49:31 +0530

    added-create-contact

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_UsingCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome To Address Book System!");
+            AddrBook.GetCustomer();
+            AddressBook_UsingCollection.AddrBook.Modify();
+            AddrBook.ListingPeople();
+
+        }
+    }
+}
