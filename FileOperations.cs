12e633366d3ee34e6cc809cbec74eeeaf2084631
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-08 21:20:06 +0530

    UC14-Csv-Operation

--- FileOperations.cs
+++ FileOperations.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AddressBook_UsingCollection
 {
     internal class FileOperations
     {
+
+        const string filepath = @"D:\Random Programs\Address_Book\Address_Book\DictFile.txt";
+
+        //Write content to file
+        public static void GetDictionary(Dictionary<string, List<AddrBook>> addressbooknames)
+        {
+            //Reset the file as empty
+            File.WriteAllText(filepath, string.Empty);
+            //Iterate over each AddressBook in Dictionary
+            foreach (KeyValuePair<string, List<AddrBook>> kvp in addressbooknames)
+            {
+                //Append key in file
+                File.AppendAllText(filepath, "Address Book: " + kvp.Key + Environment.NewLine);
+                foreach (var member in kvp.Value)
+                {
+                    //Append Values or Contacts in file
+                    File.AppendAllText(filepath, member.ToString() + Environment.NewLine);
+                }
+                //Enter a new line to File
+                File.AppendAllText(filepath, Environment.NewLine);
+                Console.WriteLine("----------The Content written in the file----------");
+                //Read content present in File
+                ReadAddressBook();
+            }
+
+        }
+        public static void ReadAddressBook()
+        {
+            try
+            {
+                //Read All content of File
+                string[] text = File.ReadAllLines(filepath);
+                foreach (var mem in text)
+                    Console.WriteLine(mem);
+                Console.WriteLine("\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
